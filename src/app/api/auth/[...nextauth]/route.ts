429a7c1bd2d328769e69c011ca65ab49d60c3638
admin <rsgonzaga@gmail.com>
2025-01-05 17:49:51 -0300

    alterando o nome das variaveis de ambiente sem o prefixo AWS

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -13,8 +13,8 @@ function calculateSecretHash(username: string) {
 const cognitoClient = new CognitoIdentityProviderClient({
   region: process.env.COGNITO_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+    accessKeyId: process.env.ACCESS_KEY_ID_AWS || "",
+    secretAccessKey: process.env.SECRET_ACCESS_KEY_AWS || "",
   },
 });
 
--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -12,8 +12,8 @@ function calculateSecretHash(username: string) {
 const cognitoClient = new CognitoIdentityProviderClient({
   region: process.env.COGNITO_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+    accessKeyId: process.env.ACCESS_KEY_ID_AWS || "",
+    secretAccessKey: process.env.SECRET_ACCESS_KEY_AWS || "",
   },
 });
 
